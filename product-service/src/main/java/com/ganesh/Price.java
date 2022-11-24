8c35d0893c436bb1799a5ec6c417885ce42055e2
GANESHKUMAR <ganesh9ot@gmail.com>
2022-11-24 23:26:14 +0530

    # adding restTemplate in product-service with Inventory and Price Model

---
+++ product-service/src/main/java/com/ganesh/Price.java
@@ -0,0 +1,17 @@
+package com.ganesh;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Price {
+	
+	private Long priceId;
+	private Long productId;
+	private Integer originalPrice;
+	private Integer discountedPrice;
+
+}
--- product-service/src/main/java/com/ganesh/ProductController.java
+++ product-service/src/main/java/com/ganesh/ProductController.java
@@ -3,12 +3,17 @@ package com.ganesh;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class ProductController {
+	
+	@Autowired
+	private RestTemplate restTemplate;
 	
 	List<ProductInfo> productList = new ArrayList<ProductInfo>();
 	
@@ -17,7 +22,15 @@ public class ProductController {
  		// 1. Get Name and Description From Product Service
 		ProductInfo info = getProductInfo(productId);
 		
-		return new Product(info.getProductId(), info.getProductName(), info.getProductDescription(), 999, true);
+		// 2. Getting price from pricing service
+		Price price = restTemplate.getForObject("http://localhost:7771/price/"+ productId, Price.class);
+		
+		// 3. Getting stock from inventory service
+		Inventory inventory = restTemplate.getForObject("http://localhost:7772/inventory/"+ productId, Inventory.class);
+		
+		//return new Product(info.getProductId(), info.getProductName(), info.getProductDescription(), 999, true);
+		return new Product(info.getProductId(), info.getProductName(), info.getProductDescription(), 
+				price.getDiscountedPrice(), inventory.getInStock());
 	}
 
 	private ProductInfo getProductInfo(Long productId) {
